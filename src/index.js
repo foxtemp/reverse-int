496ea3104ab55e903c096362f2ecef2528e94651
Serhey Grishin <foxtemp@ya.ru>
2022-06-30 16:23:53 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = num.toString();
+    let result = '';
+    for (i=0; i < str.length; i++) {
+      result = str[i]+result;
+    }
+    return result;
 }
