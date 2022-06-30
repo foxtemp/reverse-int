ba3545d6bab001d15a2c60ed1955ec5cc46d0a77
Serhey Grishin <foxtemp@ya.ru>
2022-06-30 17:00:43 +0300

    feat: add work with (-)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 module.exports = function reverse (n) {
-    let str = num.toString();
-    let result = '';
-    for (i=0; i < str.length; i++) {
-      result = str[i]+result;
-    }
-    return result;
+    console.log(num);
+  console.log(num.toString().length);
+  let str = num.toString();
+  let result = '';
+  if (num >= 0) {  
+  for (i=0; i < str.length; i++) {
+    //console.log(result);
+    result = str[i]+result;
+     }
+  console.log(result);
+  return result;
+  }
+  else {
+
+for (i=1; i < str.length; i++) {
+    //console.log(result);
+    result = str[i]+result;
+     }
+  console.log(-result);
+  return -result;
+  }
 }
